dbc391b53abb632e0f96a3b25e79a9ccf8a8383a
Fco. Javier Cordoba <i72coref@uco.es>
2020-05-27 17:53:48 +0200

    Nueva Estructura

--- src/mdas/implementacion/usuarios/IUsuarioMgt.java
+++ src/mdas/implementacion/usuarios/IUsuarioMgt.java
@@ -1,5 +1,12 @@
 package mdas.implementacion.usuarios;
 
 public interface IUsuarioMgt {
+	
+	public String getUser();
+	public void setUser(String user);
+	public String getPassword();
+	public void setPassword(String password);
+	public String iniciarSesion();
+	boolean registroUsuario2();
 
 }
